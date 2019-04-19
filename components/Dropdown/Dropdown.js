2bf2b2afcb075160adef3e88c1473e579ab85f45
schang1146 <sammychang2185@gmail.com>
2019-04-18 20:22:26 -0700

    tried to implement a sliding dropdown menu with GSAP

--- components/Dropdown/Dropdown.js
+++ components/Dropdown/Dropdown.js
@@ -7,22 +7,34 @@ class Dropdown {
     // Get the element with the ".dropdown-button" class found in the dropdown element (look at the HTML for context)
     this.button = element.querySelector('.dropdown-button');
     
-    // assign the reference to the ".dropdown-content" class found in the dropdown element
+    // Assign the reference to the ".dropdown-content" class found in the dropdown element
     this.content = element.querySelector('.dropdown-content');
     
     // Add a click handler to the button reference and call the toggleContent method.
     this.button.addEventListener('click', () => {
       this.toggleContent(this.content)
-    })
+    });
   }
 
   toggleContent() {
     
     // Toggle the ".dropdown-hidden" class off and on
-    this.content.classList.toggle('dropdown-hidden');
+    // this.content.classList.toggle('dropdown-hidden');
+
+    // Create timeline with GSAP
+    if (!this.tl) {
+      this.tl = new TweenMax(this.content);
+      this.tl = TweenMax.to(this.content, 1, {className: "-=dropdown-hidden"});
+    }
+    
+    // Sliding dropdown menu with GSAP
+    if (this.content.classList.contains('dropdown-hidden')) {
+      this.tl.play();
+    } else {
+      this.tl.reverse();
+    }
   }
 }
-
 
 // Nothing to do here, just study what the code is doing and move on to the Dropdown class
 let dropdowns = document.querySelectorAll('.dropdown').forEach( dropdown => new Dropdown(dropdown));
